afa5587016123aecaba5b563b69ff597e34643ff
Matthew Jiang <matthewljiang@gmail.com>
2017-07-26 19:24:46 -0400

    Set up react

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
+from flask import send_file, make_response, render_template
 from api.api import api
 import db.db
 
-app = Flask(__name__, static_url_path='/dist', static_folder='dist')
+app = Flask(__name__, static_url_path='/dist', static_folder='client/dist')
 
 app.register_blueprint(api, url_prefix='/api')
 
 @app.route('/')
-
+@app.route('/<path:p>')
 def ui(p=None):
     return render_template('index.html'), 200
 
