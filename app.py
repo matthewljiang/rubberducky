ff1b921abb96235a90cd4c077a966f3a94822b8c
Matthew Jiang <matthewljiang@gmail.com>
2017-07-26 19:33:09 -0400

    Update app.py and add pycache files into gitignore

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import send_file, make_response, render_template
 from api.api import api
 import db.db
 
-app = Flask(__name__, static_url_path='/dist', static_folder='client/dist')
+app = Flask(__name__)
 
 app.register_blueprint(api, url_prefix='/api')
 
 @app.route('/')
 @app.route('/<path:p>')
 def ui(p=None):
-    return render_template('index.html'), 200
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000)
