d2a2c38b4cbad98541036222be96acb6ee82e1df
ap3ca <akshat.pendharkar@hotmail.com>
2017-07-28 16:03:20 -0400

    added legislator and social media endpoints

--- api/api.py
+++ api/api.py
@@ -1,5 +1,12 @@
 from flask import Blueprint
 import requests
+from pymongo import MongoClient
+import json
+from flask import jsonify,request
+from bson.json_util import dumps
+
+c = MongoClient()
+db = c.rubberducky
 
 api = Blueprint('api', __name__)
 
@@ -12,3 +19,20 @@ def request_test():
     payload = {'current': 'true'}
     response = requests.get('https://www.govtrack.us/api/v2/role', params=payload)
     return response.content
+
+@api.route('/legislators/current', methods=["GET"])
+def current_legislators():
+	legislators = dumps(db.legislators_current.find())
+	return legislators
+
+@api.route('/legislator', methods=["GET"])
+def specific_legislator():
+	bioguide = request.args.get('bioguide')
+	legislators = dumps(db.legislators_current.find({"id.bioguide": bioguide}))
+	return legislators
+
+@api.route('/socialmedia', methods=["GET"])
+def specific_socialmedia():
+	bioguide = request.args.get('bioguide')
+	legislators = dumps(db.legislators_social_media.find({"id.bioguide": bioguide}))
+	return legislators
--- db/db.py
+++ db/db.py
@@ -7,28 +7,28 @@ db = c.rubberducky
 
 #create or update legislators
 legislators_current = db['legislators_current']
-with open('congress-legislators/legislators-current.json') as file:
+with open('db/congress-legislators/legislators-current.json') as file:
 	legislators_current_json = json.load(file)
 	for item in legislators_current_json:
 		legislators_current.update_one({"id.bioguide":item["id"]["bioguide"]},{"$set":item},upsert=True)
 
 #create or update social media
 legislators_social_media = db['legislators_social_media']
-with open('congress-legislators/legislators-social-media.json') as file:
+with open('db/congress-legislators/legislators-social-media.json') as file:
 	legislators_social_media_json = json.load(file)
 	for item in legislators_social_media_json:
 		legislators_social_media.update_one({"id.bioguide":item["id"]["bioguide"]},{"$set":item},upsert=True)
 
 #create or update committees
 committees_current = db['committees_current']
-with open('congress-legislators/committees-current.json') as file:
+with open('db/congress-legislators/committees-current.json') as file:
 	committees_current_json = json.load(file)
 	for item in committees_current_json:
 		committees_current.update_one({"thomas_id":item["thomas_id"]},{"$set":item},upsert=True)
 
 #create or update committee member lists
 committee_membership_current = db['committee_membership_current']
-with open('congress-legislators/committee-membership-current.json') as file:
+with open('db/congress-legislators/committee-membership-current.json') as file:
 	committee_membership_current_json = json.load(file)
 	for item in committee_membership_current_json:
 		com = {}
