f71d05dd936a74c136f1f71e601f5f13e932fce7
Puja Soni <pds7yx@virginia.edu>
2017-08-02 22:26:16 -0400

    api for committees

--- api/api.py
+++ api/api.py
@@ -36,3 +36,15 @@ def specific_socialmedia():
 	bioguide = request.args.get('bioguide')
 	legislators = dumps(db.legislators_social_media.find({"id.bioguide": bioguide}))
 	return legislators
+
+@api.route('/committees/current', methods=["GET"])
+def current_committees():
+	committees = dumps(db.committees_current.find({}, {"name": 1, "thomas_id": 1, "_id": 0 }))
+	return committees
+
+@api.route('/committee', methods=['GET'])
+def specific_committee():
+	committee_id = request.args.get('thomas_id')
+	#theres no committee name in this collection, only the abbreviation.
+	committee = dumps(db.committee_membership_current.find({"thomas_id":thomas_id}, {"thomas_id": 1, "members.name":1, "subcommittees":1, "_id": 0}))
+	return committee
