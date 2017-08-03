3da47b2022811f978dc0b7e44aab78cfa52adef7
Brian Chung <bchung1@umd.edu>
2017-08-03 16:02:26 -0400

    data added

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,14 +1,18 @@
 chrome.browserAction.onClicked.addListener(function(tab) { 
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		$.ajax({
-  				url: "localhost:5000/api/legislators/names",
-  				data: data,
-  				success: success,
-  				dataType: dataType
-			});
-      	names = ["Trump", "Lamar Alexander"]
-		chrome.tabs.sendMessage(tabs[0].id, {message: "sending names", data: names}, function(response) {
+
+		$.ajax({ 
+		url: "http://localhost:5000/api/legislators/names", 
+		success: function(data) { 
+			var data_json = JSON.parse(data);
+			console.log(data_json)
+			chrome.tabs.sendMessage(tabs[0].id, {message: "sending names", data: data_json}, function(response) {
 			console.log(response);
 		});
+		} 
+		});
+		// chrome.tabs.sendMessage(tabs[0].id, {message: "sending names", data: ["Trump"]}, function(response) {
+		// 	console.log(response)
+		// });
     });
 });
--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,6 +1,8 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 		if(request.message == "sending names") {
-		names = request.data
+		data = request.data
+		var names = getNames(data)
+		var mapping = mapNamesToTerms(data);
 		$('p').highlight(names);
 		$( ".highlight" ).wrap( "<div class='popup-outer-wrapper' style='display:inline-flex' popup='False'></div>" );
 		$(".highlight").css({ backgroundColor: "#C0C0C0" });
@@ -15,31 +17,75 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 		}
 
+		function getNames(data){
+			names = []
+			for(var i = 0; i < data.length; i++){
+				names.push(data[i].name.official_full)
+			}
+			return names
+		}
+
+		function mapNamesToTerms(data){
+			mapping = {}
+			for(var i = 0; i < data.length; i++){
+				var name = data[i].name.official_full;
+				mapping["name"] = data[i].terms
+			}
+			return mapping;
+		}
 
 		$(".popup-outer-wrapper").click(function() {
-			popUpDiv = getPopUpDiv()
-			$(this).prepend(popUpDiv)	
-			$(this).attr("popup", "True")
+			var name = $(this).text();
+			var terms = mapping.name;
+			console.log(terms)
+			var popup = $(this).attr("popup");
+			var popUpDiv = getPopUpDiv(name, terms);
+			if(popup != "True"){
+				$(this).prepend(popUpDiv);
+				$(this).attr("popup", "True");
+			}
+
+			$(".icon-remove").click(function(){
+			console.log("DELETE ICON")
+			$(".popup-outer-wrapper").remove()
+			})
+
+			$("#home").click(function(){
+				window.location.href = "http://localhost:5000/ducky";
+			});
 		});
+
+		// function delegateFunc(){
+		// 	$("fa fa-times").click(function(){
+		// 		console.log($(this).parent());
+		// 	});
+		// }
 
 		console.log("Done Highlighting")
 		sendResponse("Done highlighting names")
 	}
 });
 
-function getPopUpDiv(){
+function getPopUpDiv(name, terms){
+	var state = terms[0].state;
+	var party = terms[0].party;
+	var type = (terms[0].type == "rep") ? "Representative" : "Senator";
 	// return '<div class=popup style=\"position:absolute; background-color: whitesmoke; padding:10px; border:1px solid black; border-radius:10px\"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris malesuada turpis metus, vitae fermentum mauris imperdiet ut. Suspendisse aliquam </div>'
-	return '<div class=popup> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </div>'
+	return '<div class="popup"><i class="btn icon-remove"></i><div>' + type + '</div><div>' + name + '</div><div>' + state + ' - ' + party + '</div> <button id="home" type="button" class="btn btn-info center">See More</button></div>'
+
 }
 
 
-// <div class="col-md-4">
-//            <div class="outer-wrapper">
-//             <div class="on-hover-content">
-//             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris malesuada turpis metus, vitae fermentum mauris imperdiet ut. Suspendisse aliquam</p>
-//             </div>
-//             <a type="button" class="btn btn-success">A paragraph</a>
-//         </div>
+ 
+// '<div class="container-fluid popup"><div class="row">type</div><div class="row">' + name + '</div><div class="row">' + state + ' - ' + party + '</div></div>'
+
+// <div class="container-fluid popup">
+//   <div class="row">type</div>
+//   <div class="row">name</div>
+//   <div class="row">state party</div>
+// </div>
+
+
 
 
 
