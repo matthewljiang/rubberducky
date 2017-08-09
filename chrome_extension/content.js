4d93b4e6cbf049107ffa7f812ddf9dd526fc80a4
Brian Chung <bchung1@umd.edu>
2017-08-09 14:10:54 -0400

    more sadness

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -27,20 +27,31 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 		function mapNamesToTerms(data){
 			mapping = {}
+			var name; 
+			var terms;
+			var id;
+			var newobj;
 			for(var i = 0; i < data.length; i++){
-				var name = data[i].name.official_full;
-				var terms = data[i].terms;
-				terms["id"] = data[0].id.bioguide;
-				mapping["name"] = terms
+				id = data[i].id.bioguide;
+				name = data[i].name.official_full;
+				terms = data[i].terms;
+				newobj = {}
+				newobj["id"] = id;
+				newobj['name'] = name
+				newobj['terms'] = terms
+				mapping[name] = newobj
 			}
 			return mapping;
 		}
 
 		$(".popup-outer-wrapper").click(function() {
 			var name = $(this).text();
-			var terms = mapping.name;
+			console.log(name)
+			console.log(mapping[name])
+			console.log(mapping)
+			var obj = mapping[name];
 			var popup = $(this).attr("popup");
-			var popUpDiv = getPopUpDiv(name, terms);
+			var popUpDiv = getPopUpDiv(name, obj);
 			if(popup != "True"){
 				$(this).prepend(popUpDiv);
 				$(this).attr("popup", "True");
@@ -49,11 +60,12 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 			$(".icon-remove").click(function(){
 				console.log("DELETE ICON")
 				$(".popup").remove()
+				$(".popup-outer-wrapper").attr("popup", "False");
 			})
 
 			$("#home").click(function(){
 				var id = $(this).attr("bioid")
-				var link = "http://localhost:5000/ducky/legislator/P000612";
+				var link = "http://localhost:5000/ducky/legislator/" + id;
 				console.log(link)
 				window.open(link, "_blank");
 			});
@@ -70,13 +82,13 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	}
 });
 
-function getPopUpDiv(name, terms){
-	var state = terms[0].state;
-	var party = terms[0].party;
-	var type = (terms[0].type == "rep") ? "Representative" : "Senator";
-	var id = terms.id
+function getPopUpDiv(name, obj){
+	var state = obj.terms[0].state;
+	var party = obj.terms[0].party;
+	var type = (obj.terms[0].type == "rep") ? "Representative" : "Senator";
+	var id = obj.id
 	// return '<div class=popup style=\"position:absolute; background-color: whitesmoke; padding:10px; border:1px solid black; border-radius:10px\"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris malesuada turpis metus, vitae fermentum mauris imperdiet ut. Suspendisse aliquam </div>'
-	return '<div class="popup"><i class="btn icon-remove"></i><div>' + "Senator" + '</div><div id="name">' + name + '</div><div>' + "GA" + ' - ' + "Republican" + '</div> <button id="home" bioid=' + '\"' + id + '\"' + 'type="button" class="btn btn-info center">See More</button></div>'
+	return '<div class="popup"><i class="btn icon-remove"></i><div>' + type + '</div><div id="name">' + name + '</div><div>' + state + ' - ' + type + '</div> <button id="home" bioid=' + '\"' + id + '\"' + 'type="button" class="btn btn-info center">See More</button></div>'
 
 }
 
